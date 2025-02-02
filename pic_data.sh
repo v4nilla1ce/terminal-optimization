bb2f217b486d13783e4b442f2d325e60a217364e
v4nilla1ce <v4nilla@tuta.io>
2025-02-02 22:08:26 +0100

    readme

--- pic_data.sh
+++ pic_data.sh
@@ -1,12 +1,4 @@
-# terminal-optimization
-```
-brew install exiftool
-````
-```
-nano ~/pic_data.sh
-```
 #!/bin/bash
-```
 
 if [ -z "$1" ]; then
     echo "Usage: pic_data <path_to_picture>"
@@ -21,13 +13,3 @@ else
     echo "Error: File not found at path '$file_path'"
     exit 1
 fi
-```
-```
-chmod +x ~/pic_data.sh
-```
-```
-mv ~/pic_data.sh /usr/local/bin/pic_data
-```
-```
-pic_data "/path/to/your/image.jpg"
-```
